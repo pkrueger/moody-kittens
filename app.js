ee6361bb7ebd597c4e405ffc2ffba81dff9385a6
Patrick Krueger <sendthistopatrick@gmail.com>
2022-07-17 01:31:22 -0600

    create kitten game functionality + basic cat pics

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let kittens = []
-const moods = ['happy', 'tolerant', 'angry', 'gone']
-let baseMood = 'tolerant'
-let baseAffection = 41
+const baseMood = 'tolerant'
+const baseAffection = 41
+const maxAffection = 100 
 /* 0 = gone, 1-40 = angry, 41-80 = tolerant, 80-100 = happy
 /**
  * Called when submitting the new Kitten Form
@@ -12,20 +12,27 @@ let baseAffection = 41
  */
 function addKitten(event) {
   event.preventDefault()
-  let kittenName = event.target.name
+  let kittenName = event.target.name.value
+  let newKitten = {}
 
-  if(kittens.findIndex(kitten => kitten.name == kittenName) == -1) {
-    let kitten = {
+  if(kittens.length == 0) {
+    newKitten = {
       id: generateId(),
       name: kittenName,
       mood: baseMood,
       affection: baseAffection
     }
-  } else {
+    kittens.push(newKitten)
+  } else if(kittens.findIndex(kitten => kitten.name == kittenName) == -1) {
+      newKitten = {
+        id: generateId(),
+        name: kittenName,
+        mood: baseMood,
+        affection: baseAffection
+      }
+      kittens.push(newKitten)
+    } 
 
-  }
-
-  kittens.push(kitten)
   saveKittens()
   event.target.reset()
 }
@@ -36,6 +43,7 @@ function addKitten(event) {
  */
 function saveKittens() {
   window.localStorage.setItem('kittens', JSON.stringify(kittens))
+  drawKittens()
 }
 
 /**
@@ -49,23 +57,23 @@ function loadKittens() {
   if(localKittens) {
     kittens = localKittens
   }
-  drawKittens()
 }
 
 /**
  * Draw all of the kittens to the kittens element
+ * the kitten id is added to image class list
  */
 function drawKittens() {
   let template = ""
-  
 
-  for(let kitten of kittens) {`
-    <div class="card">
-      <img TODO:>
-      <div>${kitten.name}</div>
-      <div>${kitten.mood}</div>
-    </div>
-    `
+  for(let kitten of kittens) {
+    template += `
+      <div class="card">
+        <img TODO>
+        <div>${kitten.name}</div>
+        <div>${kitten.mood}</div>
+      </div>
+      `
   }
 }
 
@@ -80,7 +88,7 @@ function findKittenById(id) {
   if(index == -1) {
     throw new Error('Invalid Kitten ID')
   } else {
-    return index
+    return kittens[index]
   }
 }
 
@@ -94,10 +102,21 @@ function findKittenById(id) {
  * @param {string} id 
  */
 function pet(id) {
-  let randomNumber = Math.random()
-  if(randomNumber > .5) {
+  let importedKitten = findKittenById(id)
+  let randomNum = Math.random()
+  let affectionIncrease = Math.floor((Math.random() * 8) + 6)
 
+  if(randomNum > .5) {
+    if(importedKitten.affection + affectionIncrease > maxAffection) {
+      importedKitten.affection = 100
+    } else{
+      importedKitten.affection += affectionIncrease
+    }
+  } else {
+    importedKitten.affection -= 5
   }
+  setKittenMood(importedKitten)
+  saveKittens()
 }
 
 /**
@@ -107,6 +126,10 @@ function pet(id) {
  * @param {string} id
  */
 function catnip(id) {
+  let importedKitten = findKittenById(id)
+  importedKitten.affection = 50
+  setKittenMood(importedKitten)
+  saveKittens()
 }
 
 /**
@@ -114,6 +137,25 @@ function catnip(id) {
  * @param {Kitten} kitten 
  */
 function setKittenMood(kitten) {
+  let toggleClass = document.getElementsByClassName(JSON.stringify(kitten.id)).toggle(kitten.mood)
+  
+  if(kitten.affection == 0) {
+    toggleClass
+    kitten.mood = "gone"
+    toggleClass
+  } else if(kitten.affection < 41) {
+    toggleClass
+    kitten.mood = "angry"
+    toggleClass
+  } else if(kitten.affection < 81) {
+    toggleClass
+    kitten.mood = "tolerant"
+    toggleClass
+  } else {
+    toggleClass
+    kitten.mood = "happy"
+    toggleClass
+  }
 }
 
 /**
@@ -132,6 +174,7 @@ function clearKittens(){
 function getStarted() {
   document.getElementById("welcome").remove();
   console.log('Good Luck, Take it away')
+  drawKittens()
 }
 
 
--- readme.md
+++ readme.md
@@ -27,14 +27,14 @@ The basic design and layout for this app has been mocked out and the names of th
 
 ## Requirements: 
 - [x] Users must have a form where they enter in at minimum the name of a kitten 
-- [o] A Kitten's name and mood must be visible for the user 
+- [ ] A Kitten's name and mood must be visible for the user 
 - [x] A custom Google Font must be added 
 - [ ] A Kitten's mood must affect visually the appearance of the kitten 
 - [x] Add Kitten form clears when submitted 
 - [x] Add Kitten form submission does not reload the page 
 - [x] Kittens are stored in local storage 
-- [o] Kittens can be deleted 
-- [ ] Kittens have at least two buttons that affect the kitten's mood in different ways 
-- [ ] Kitten's and their mood persist through page reloads 
+- [x] Kittens can be deleted 
+- [x] Kittens have at least two buttons that affect the kitten's mood in different ways 
+- [x] Kitten's and their mood persist through page reloads 
 - [x] Kitten's with the same name as an already present kitten cannot be added 
 - [x] Kittens are visible when the page reloads
